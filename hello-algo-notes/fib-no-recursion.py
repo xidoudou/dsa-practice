5fbc019fa72cb630773f4bdf898e8ae21b491d2a
xidoudou <yangxi_dd@yahoo.com>
2026-08-25 10:23:19 +0200

    fib

---
+++ hello-algo-notes/fib-no-recursion.py
@@ -0,0 +1,14 @@
+def fib(n:int) -> int:
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    a, b = 0 ,1
+    for i in range(2,n+1):
+        a, b = b, a+b
+    return b
+
+            
+
+    
--- hello-algo-notes/fib.py
+++
@@ -1,7 +0,0 @@
-def fib(n:int) -> int:
-    if n ==1 or n ==2:
-        return n - 1
-    else:
-        res = fib(n-1) + fib(n-2)
-    return res
-    
